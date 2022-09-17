3fc6e47cd2a2ac74b5628c3cb876263d8b6c6d2e
Magdalena <magdalenamalek52@gmail.comgit statusgit status git config --global user.email magdalenamalek52@gmail.com>
2022-09-17 13:12:25 +0200

    add dark mode

--- server.js
+++ server.js
@@ -17,7 +17,9 @@ app.get('/about', (req, res) => {
 })
 
 app.get('/contact', (req, res) => {
-    res.render('contact');
+    res.render('contact', {
+        layout: 'dark'
+    });
 })
 
 app.get('/info', (req, res) => {
